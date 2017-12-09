a0ccaf82b8d22cb024b61dfe0aa4a0497efabe89
Jenny Gao <jgao658@gmail.com>
2017-12-09 13:55:44 -0500

    finished writing all the functions and added comments

--- intro.js
+++ intro.js
@@ -1,10 +1,44 @@
 var button=document.getElementById("b");
-button.addEventListener("click",addToList);
+var listTag=document.getElementsByTagName("LI");
 
+//adds an element to the list when button is clicked
 var addToList=function(e){
     var list=document.getElementById("thelist");
     var item=document.createElement("LI");
-    var text=document.createTextNode("new item");
-    item.appendChild(text);
+    item.innerHTML="item "+listTag.length;
     list.appendChild(item);
+    addListeners(item); //add event listeners to newly created list element
+};
+
+//changes the heading at the top to contain the text of the item when mouse goes over the item
+var changeHeading=function(e){
+    var heading=document.getElementById("h");
+    heading.innerHTML=this.innerHTML;
+};
+
+//heading goes back to "Hello World" when the mouse is no longer over an item in the list
+var revert=function(e){
+    var heading=document.getElementById("h");
+    heading.innerHTML="Hello World!";
+};
+
+//removes an item on the list if clicked
+var removeListItem=function(e){
+    this.remove()
+};
+
+//add event listener to button
+button.addEventListener("click",addToList);
+
+//this function adds event listeners to items
+var addListeners=function(element){
+    element.addEventListener("mouseover", changeHeading);
+    element.addEventListener("mouseout", revert);
+    element.addEventListener("click", removeListItem);
+};
+
+//add event listeners to list items
+var listList = document.getElementsByTagName("LI");
+for (i=0;i<listList.length;i++){
+    addListeners(listList[i]);
 };
